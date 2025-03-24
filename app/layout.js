89e97301f8284b3c639c8ec7854e1800264e29eb
czeslaw187 <czeslaw187@gmail.com>
2025-03-24 07:14:34 +0000

    next image codemod

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Image from "next/image";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -21,6 +22,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} w-screen min-h-screen bg-gradient-to-b from-slate-900 via-cyan-700 to-amber-500`}>
+        
         {children}
       </body>
     </html>
